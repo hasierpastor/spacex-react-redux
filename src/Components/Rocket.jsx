e67eab0fe6ae42f3ecad9e39a577b7573dc3569a
Hasier Pastor <hasier.pastor@gmail.com>
2019-05-03 15:07:13 -0700

    added all images to rocket component:

--- src/Components/Rocket.jsx
+++ src/Components/Rocket.jsx
@@ -4,11 +4,15 @@ import '../../styles/_style.sass'; // Css-module styles
 
 const Rocket = props => {
   const { name, description, images } = props;
+  let imageContent = images.map(image => {
+    return <img src={image} alt="" />;
+  });
+
   return (
     <div id="rocket">
       <h2> {name} </h2>
       <p> {description} </p>
-      <img srcSet={images} />
+      <div>{imageContent}</div>
     </div>
   );
 };
